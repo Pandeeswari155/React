8b26f20978cd923f41c2b617d19ea83f1c37fb22
Pandeeswari155 <727723eucs155@skcet.ac.in>
2024-08-12 19:01:26 +0530

    DAY2CS2

---
+++ day2cs2.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Inline(){
+    return(
+        <div style={{backgroundcolor:"lightblue",color:"blue"}}>
+            <h1 style={{color:"green"}}>Inline</h1>
+            <p style={{color:"darkblue",fontsize:"16px"}}>This is a inline style applied.</p>
+        </div>
+    )
+};
+export default Inline;
