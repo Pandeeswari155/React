cfd63e87e75552616f5d47bc89ba29bce0c59600
Pandeeswari155 <727723eucs155@skcet.ac.in>
2024-08-20 20:21:38 +0530

    Update ch2.jsx

--- Day2/ch2.jsx
+++ Day2/ch2.jsx
@@ -1,1 +1,26 @@
 
+
+let directions = ["North", "South", "East", "West"];
+    function ref()
+    {
+        document.write(directions);
+    }
+    ref()
+    function Add() {
+      directions = directions.map(direction => direction + " News");
+      document.write(directions);
+      
+      <div>
+        <button onClick={ref}>ref</button>
+      </div>
+    }
+    function D()
+    {
+        return(
+            <div  className="arrayDisplay">
+                <button onClick={Add}>Add</button>
+                
+            </div>
+        )
+    }export default D; can't understand
+      
